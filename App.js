0a307f0fc31cdc5112a56abbdf86aadd96d0d4b4
SophiaSantos17 <santosvsophia17@gmail.com>
2024-03-12 17:08:28 -0300

    Feat: chamando o botão

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import Button from "./scr/components/button";
+import { StyleSheet, View } from "react-native";
+import colors from "./scr/styles/colors";
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.containerApp}>
+    <Button text="Calcular" backgound={colors.white} color={colors.red_primary} border={colors.red_primary}/>
+    <Button text="Acessar" backgound={colors.red_primary} color={colors.white} border={colors.white}/>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerApp: {
+    fontFamily: "Inter",
   },
-});
+})
+

9e912619e31da3d490e7da482caa6bf61ac63374
SophiaSantos17 <santosvsophia17@gmail.com>
2024-03-12 17:05:43 -0300

    Feat: configuração das cores

---
+++ scr/styles/colors.js
@@ -0,0 +1,13 @@
+
+// definindo os valores padrões das cores
+const colors = {
+  red_primary: "#D61616",
+  white: "#fff",
+  black: "#000",
+  dark_white: '#FCFCFC',
+  dark_gray: "#4B4B4B"
+}
+
+
+// expostando as cores
+export default colors;

99a4a39a4c6dc81517725728bb410ba2b8359b7f
SophiaSantos17 <santosvsophia17@gmail.com>
2024-03-12 17:06:01 -0300

    Feat: configruação dos valores das fontes

---
+++ scr/styles/fonsts.js
@@ -0,0 +1,15 @@
+
+
+// Definir valores padrões das fontes
+const fonts = {
+  regular: 18,
+  small: 15,
+  big: 25,
+  tiny: "300",
+  bold: "800",
+  medium: '600',
+  button: '600',
+};
+
+// Exportar as fontes
+export default fonts;

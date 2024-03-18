771f1257763813f78efc57f6a16edffb4edd9367
Lucas Vasconcelos <lucas.jesus44@gmail.com>
2024-03-18 17:09:02 -0300

    Feat: created topBar component

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, View } from "react-native";
 import colors from "./scr/styles/colors";
 import Header from "./scr/components/header";
 import icons from "./scr/styles/icons";
+import PagEventos from "./scr/pages/eventos";
+import TopBar from "./scr/components/topBar";
 
 const App = () => {
   // const [selected, setSelected] = useState([false, false, false, false]);
@@ -15,14 +17,22 @@ const App = () => {
 
   return (
     <View style={styles.containerApp}>
-      <Header />
+      <TopBar
+        title={"Acompanhamentos"}
+        subtitle={
+          "Arroz, farofa e pão são obrigatórios, mas os outros, escolha de acordo com o desejavél"
+        }
+        color_button={"white"}
+        subtitleColor={colors.dark_white}
+        titleColor={colors.white}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   containerApp: {
-    backgroundColor: colors.white,
+    backgroundColor: colors.red_primary,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",

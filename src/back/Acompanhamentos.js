c2c007c03bb89b8a901c15908714da33198a2a4c
Yasmim Nascimento <yasmim.ca19@gmail.com>
2024-05-26 00:00:33 -0300

    Update Acompanhamentos.js

--- src/back/Acompanhamentos.js
+++ src/back/Acompanhamentos.js
@@ -1,3 +1,35 @@
 export default class Acompanhamentos{
-    static acompanhamentos = {'Arroz': true, 'Farofa': true, 'Pão Francês': true, 'Pão de Alho': false}
+    static arroz = true;
+    static farofa = true; 
+    static pfrances = true;
+    static paoDeAlho = false;
+    // static acompanhamentos = {'Arroz': true, 'Farofa': true, 'Pão Francês': true, 'Pão de Alho': false}
+
+    static toggleArroz(){
+        Acompanhamentos.arroz = !Acompanhamentos.arroz
+        console.log(Acompanhamentos.getValores())
+    }
+
+    static toggleFarofa(){
+        Acompanhamentos.farofa = !Acompanhamentos.farofa
+        console.log(Acompanhamentos.getValores())
+    }
+
+    static togglePFrances(){
+        Acompanhamentos.pfrances = !Acompanhamentos.pfrances
+        console.log(Acompanhamentos.getValores())
+    }
+
+    static togglePaoDeAlho(){
+        Acompanhamentos.paoDeAlho = !Acompanhamentos.paoDeAlho
+        console.log(Acompanhamentos.getValores())
+    }
+    static getValores() {
+        return {
+            arroz : Acompanhamentos.arroz,
+            farofa: Acompanhamentos.farofa,
+            pfrances: Acompanhamentos.pfrances,
+            paoDeAlho : Acompanhamentos.paoDeAlho,
+        };
+    }
 }

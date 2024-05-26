d120c87d1643f3bcb3aa63f501eb947832e8f09f
Yasmim Nascimento <yasmim.ca19@gmail.com>
2024-05-26 00:02:23 -0300

    Update Utensilios.js

--- src/back/Utensilios.js
+++ src/back/Utensilios.js
@@ -1,3 +1,48 @@
 export default class Utensilios{
-    static utensilios = {'Carvão': false, 'Prato': false, 'Kit Talher': false, 'Copos': false, 'Guardanapo': false}
+    static carvao = false;
+    static prato = false; 
+    static kittalher = false; 
+    static copos = false;
+    static guardanapo = false; 
+
+    static toggleCarvao() {
+        Utensilios.carvao = !Utensilios.carvao 
+        console.log(Utensilios.getValores())
+    }
+
+    static togglePrato() {
+        Utensilios.prato = !Utensilios.prato
+        console.log(Utensilios.getValores())
+    }
+
+    static toggleKittalher() {
+        Utensilios.kittalher = !Utensilios.kittalher
+        console.log(Utensilios.getValores())
+    }
+
+    static toggleCopos() {
+        Utensilios.copos = !Utensilios.copos
+        console.log(Utensilios.getValores())
+    }
+
+    static toggleGuardanapo() {
+        Utensilios.guardanapo = !Utensilios.guardanapo
+        console.log(Utensilios.getValores())
+    }
+
+    static getValores(){
+        return{
+            carvao: Utensilios.carvao, 
+            prato: Utensilios.prato, 
+            kittalher: Utensilios.kittalher, 
+            copos: Utensilios.copos, 
+            guardanapo: Utensilios.guardanapo,
+        };
+    }
+
+    // static toggle() {
+    //     Utensilios. = !Utensilios. 
+    //     console.log(Utensilios.getValores())
+    // }
+    
 }

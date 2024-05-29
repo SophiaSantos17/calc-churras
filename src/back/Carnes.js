ba5415c58c0da9c9b842db35ffcd728a1c6d0e08
karol <karolsilvano3@gmail.com>
2024-05-29 17:24:45 -0300

    feat-slaaa

--- src/back/Carnes.js
+++ src/back/Carnes.js
@@ -2,4 +2,15 @@ export default class Carnes{
     static bovina = {'Picanha': false, 'Maminha': false, 'Costela': false}
     static suina = {'Pernil': false, 'Linguica':false, 'Costelinha': false}
     static frango = {'Asa': false, 'Peito': false, 'Coxa': false}
+    
+    static retornarListas(){
+        console.log('TESTess', Carnes.bovina)
+        // return{
+        //     bovina: Carnes.bovina,
+        //     suina: Carnes.suina,
+        //     frango : Carnes.frango
+        // } 
+
+    }
 }
+
--- src/pages/add-horas.js
+++ src/pages/add-horas.js
@@ -1,147 +1,5 @@
-// import React, { useState } from "react";
-// import { StyleSheet, View, ScrollView } from "react-native";
-// import { useNavigation } from '@react-navigation/native';
-// // import TextInter from "../styles/Text";
-
-// //import componentes prontod
-// import TopBar from "../components/topBar";
-// import CardText from "../components/cardText";
-// import Button from "../components/button"
-
-// // import valores
-// import colors from "../styles/colors";
-// import values from "../styles/values";
-
-// const PageAddHoras = () => {
-//   const navigate = useNavigation();
-
-//   // funcao para mudar a cor no front
-//   const [selectedOption, setSelectedOption] = useState(null);
-
-//   const handleSelection = (option) => {
-//     setSelectedOption(option);
-//   };
-
-//   const handleNext = () => {
-//     if (selectedOption === null) {
-//       Alert.alert("Aviso", "Por favor, selecione uma opção antes de continuar.");
-//     } else {
-//       navigate.navigate("NotaFiscal");
-//     }
-//   };
-
-//   const calcularCarne = () => {
-//     const baseHomem = 600;
-//     const baseMulher = 400;
-//     const additionalPer2Hours = 100;
-
-//     let hours = parseInt(selectedOption);
-//     let additional = Math.floor((hours - 4) / 2) * additionalPer2Hours;
-
-//     let meatHomens = numHomens * (baseHomem + additional);
-//     let meatMulheres = numMulheres * (baseMulher + additional);
-
-//     setResult({
-//       homens: meatHomens,
-//       mulheres: meatMulheres,
-//       total: meatHomens + meatMulheres
-//     });
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <View>
-//         <TopBar
-//           color_button={"white"}
-//           title={"Horas"}
-//           subtitle={
-//             "Selecione a quantidade de horas do churrasco"
-//           }
-//           subtitleColor={colors.white}
-//           titleColor={colors.white}
-//         />
-//       </View>
-//       <View style={styles.boxCardText}>
-//         <CardText
-//           width={"40%"}
-//           height={"25%"}
-//           text={"4 Horas"}
-//           background={selectedOption === '4' ? 'red':'red'}
-//           onPress={() => handleSelection('4')}
-//           active={selectedOption === '4'}
-//         />
-//         <CardText
-//           width={"40%"}
-//           height={"25%"}
-//           text={"6 Horas"}
-//           background={selectedOption === '6' ? 'red':'red'}
-//           onPress={() => handleSelection('6')}
-//           active={selectedOption === '6'}
-
-//         />
-//         <CardText
-//           width={"40%"}
-//           height={"25%"}
-//           text={"10 Horas"}
-//           background={selectedOption === '10' ? 'red':'red'}
-//           onPress={() => handleSelection('10')}
-//           active={selectedOption === '10'}
-
-//         />
-//         <CardText
-//           width={"40%"}
-//           height={"25%"}
-//           text={"12 Horas"}
-//           background={selectedOption === '12' ? 'red':'red'}
-//           onPress={() => handleSelection('12')}
-//           active={selectedOption === '12'}
-
-//         />
-//       </View>
-//       <View style={styles.cardButton}>
-//         <Button
-//           text="Avançar" // Titulo do botão
-//           backgound={colors.white} // Cor de fundo do botão
-//           color={colors.red_primary} // cor do botão
-//           border={colors.red_primary} // cor da borda
-//           onPress={() => navigate.navigate("NotaFiscal")} //quando pressionar, ir para outra página
-//         />
-//       </View>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: colors.red_primary,
-//     width: "100%",
-//     display: "flex",
-//     flexDirection: "column",
-//     justifyContent: "space-between",
-//     height: values.full,
-//   },
-
-//   boxCardText: {
-//     width: "100%",
-//     display: "flex",
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     gap: 4,
-//     justifyContent: "center",
-//     marginTop: -120
-//   },
-
-//   cardButton: {
-//     width: values.full,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     height: 150,
-//   },
-// });
-
-// export default PageAddHoras;
 // .........................................
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Alert, Text } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
@@ -153,15 +11,65 @@ import Button from "../components/button";
 // import valores
 import colors from "../styles/colors";
 import values from "../styles/values";
+import Convidados from "../back/Convidados";
+import Carnes from "../back/Carnes";
+import carnesAtivas from "../back/Carnes";
 
 const PageAddHoras = () => {
   const navigate = useNavigation();
 
   // Estado para controlar a opção selecionada
   const [selectedOption, setSelectedOption] = useState(null);
-  const [numHomens, setNumHomens] = useState(1);
-  const [numMulheres, setNumMulheres] = useState(2);
+  const [numHomens, setNumHomens] = useState();
+  const [numMulheres, setNumMulheres] = useState();
+  const [numCriancas, setNumCriancas] = useState();
+  const [listasCarnesAtivas, setCarnesAtivas] = useState();
   const [result, setResult] = useState(null);
+
+  useEffect(() => {
+
+    ( function definirNUm(){
+
+      const {homens, mulheres, criancas} = Convidados.getValores()
+      setNumHomens(homens)
+      setNumMulheres(mulheres)
+      setNumCriancas(criancas)
+      // setCarnesAtivas(Carnes.retornarListas())
+      console.log(Carnes.retornarListas())
+    })()
+
+
+  })
+  function listarAtivos() {
+    let ativos = [];
+
+    // const {bovina , suina , frango} = carnesAtivas
+    // console.log(carnesAtivas)
+    // // Verifica bovina
+    // for (const carne in bovina) {
+    //     if (bovina[carne]) {
+    //         ativos.push(carne);
+    //     }
+    // }
+
+    // // Verifica suina
+    // for (const carne in suina) {
+    //     if (suina[carne]) {
+    //         ativos.push(carne);
+    //     }
+    // }
+
+    // // Verifica frango
+    // for (const carne in frango) {
+    //     if (frango[carne]) {
+    //         ativos.push(carne);
+    //     }
+    // }
+    console.log(ativos)
+
+    return ativos;
+}
+
 
   const handleSelection = (option) => {
     setSelectedOption(option);
@@ -179,19 +87,30 @@ const PageAddHoras = () => {
   const calculateMeat = () => {
     const baseHomem = 600;
     const baseMulher = 400;
+    const baseCrianca = 200;
     const additionalPer2Hours = 100;
 
+    // const listaDeCarne = []
+    console.log("CHAMOU")
     let hours = parseInt(selectedOption);
-    let additional = Math.floor((hours - 4) / 2) * additionalPer2Hours;
 
-    let meatHomens = numHomens * (baseHomem + additional);
-    let meatMulheres = numMulheres * (baseMulher + additional);
 
-    const result = {
-      homens: meatHomens,
-      mulheres: meatMulheres,
-      total: meatHomens + meatMulheres
-    };
+    let totalPeso = (baseHomem * numHomens) + (baseMulher * numMulheres) + (baseCrianca * numCriancas)
+
+    totalPeso += additionalPer2Hours * hours
+
+
+
+    // let additional = Math.floor((hours - 4) / 2) * additionalPer2Hours;
+
+    // let meatHomens = numHomens * (baseHomem + additional);
+    // let meatMulheres = numMulheres * (baseMulher + additional);
+
+    
+
+    const result =  totalPeso
+    
+    console.log(totalPeso)
 
     setResult(result);
     return result;
@@ -268,13 +187,13 @@ const PageAddHoras = () => {
           onPress={handleNext}
         />
       </View>
-      {/* {result && (
+      {result && (
         <View style={styles.result}>
           <Text style={styles.resultText}>Homens: {result.homens}g</Text>
           <Text style={styles.resultText}>Mulheres: {result.mulheres}g</Text>
           <Text style={styles.resultText}>Total: {result.total}g</Text>
         </View>
-      )} */}
+      )}
     </View>
   );
 };
